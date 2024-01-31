2262ba81bbe8ec22749ddca9297e6c1b4245ed76
KYUNGAH <ukyunga0.1@gmail.com>
2024-01-31 18:30:48 +0900

    0131 update 0.1v

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-
-const writeForm = document.querySelector('.write_form');
 const list = document.querySelector('.memos');
 const li = document.querySelector('.li');
 const uu = document.querySelector('.uu');
 const searchInput = document.querySelector('#searchId');
 
-
-
+let noteData = JSON.parse(localStorage.getItem('data'));
+// noteData = JSON.parse(noteData);
 // ======================== write ========================
 const saveMemo = () => {
     for (let i = 0; i < noteData.length; i++) {
@@ -20,19 +18,31 @@ const saveMemo = () => {
             </div>
         </a>
     </li>`;
-    // liTitle.href = `detail.html?id=${noteData[i].id}`;
-    // liTitle.innerHTML = noteData[i].title            
-    // liText.innerHTML = noteData[i].title            
 
         list.innerHTML += html;
     }
 };
 
+searchInput.addEventListener('keyup', () => {
+    const searchInputValue = searchInput.value;
+    const allLi = document.querySelectorAll('.li');
+    //누른 키가 li안에 텍스트 하나라도있으면 보이고 없으면 안보이게
+    allLi.forEach((a, i) => {
+        const liText = a.textContent.toLowerCase();
 
+        const result = liText.includes(searchInputValue);
 
-// ===================================== 검색 =====================================
-
-
+        if (result) {
+            a.classList.remove('filtered');
+            uu.classList.add('none');
+            uu.classList.remove('block');
+        } else {
+            a.classList.add('filtered');
+            uu.classList.add('block');
+            uu.classList.remove('none');
+        }
+    });
+});
 
 //윈도우가 로드 됐을때.
 window.onload = () => {
@@ -42,49 +52,7 @@ window.onload = () => {
     } else {
         uu.classList.remove('block');
         uu.classList.add('none');
-        
     }
-    
+
     saveMemo();
 };
-
-
-
-writeForm.addEventListener('submit', () => {
-    const memo = writeForm.querySelector('[name="add"]').value;
-    const memo2 = writeForm.querySelector('[name="add2"]').value;
-
-    const todo = {
-        id: Date.now(),
-        title: memo,
-        text: memo2,
-    };
-    
-    noteData.push(todo);
-
-    localStorage.setItem('data', JSON.stringify(noteData));
-});
-
-let noteData = localStorage.getItem('data');
-noteData = JSON.parse(noteData);
-searchInput.addEventListener('keyup', () => {
-    const searchInputValue = searchInput.value;
-    const allLi = document.querySelectorAll('.li');
-    //누른 키가 li안에 텍스트 하나라도있으면 보이고 없으면 안보이게
-    allLi.forEach((a, i) => {
-        const liText = a.textContent;
-
-        const result = liText.includes(searchInputValue);
-
-        console.log('liText :', liText);
-        console.log('result :', result);
-
-        if (result) {
-            a.classList.remove('filtered');
-        } else {
-            a.classList.add('filtered');
-        }
-    });
-});
-
-
